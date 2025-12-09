6b3cdbb6f3dac995fd514d7f4e4be13af5f850bf
RazeSoldier52 <155234831+RazeSoldier52@users.noreply.github.com>
2025-12-09 23:02:25 +0300

    tmp

--- Assets/Scripts/Level/Flag.cs
+++ Assets/Scripts/Level/Flag.cs
@@ -1,21 +1,41 @@
+using TMPro;
 using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
-
+    [Header("UI Reference")]
+    [SerializeField] TMP_Text messageText;
+    [Header("Display Settings")]
+    [SerializeField] private float displayDuration = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<Collider2D>().enabled = false;
+
+        }
+    }
+    private System.Collections.IEnumerator DisplayMessageCoroutine()
+    {
+        if (messageText != null)
+        {
+            messageText.gameObject.SetActive(true);
+        }
+        yield return new WaitForSeconds(displayDuration);
+        if (messageText != null)
+        {
+            messageText.gameObject.SetActive(false);
+        }
     }
 }
