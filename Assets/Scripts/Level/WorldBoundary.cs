b5e111c8f4f3e3deb24fc3394bd81133d04903c0
RazeSoldier52 <155234831+RazeSoldier52@users.noreply.github.com>
2025-12-22 14:22:29 +0300

    New system

--- Assets/Scripts/Level/WorldBoundary.cs
+++ Assets/Scripts/Level/WorldBoundary.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class WorldBoundary : MonoBehaviour
 {
+    public static System.Action OnPlayerFallsOutOfBounds;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            OnPlayerFallsOutOfBounds?.Invoke();
+        }
         if(other.TryGetComponent<IBoundaryBehaviour>(out IBoundaryBehaviour handleBoundaryAction))
         {
             handleBoundaryAction.HandleBoundaryBehaviour();
