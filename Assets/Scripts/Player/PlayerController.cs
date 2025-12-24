b7574ff17a5755811c8cb4de6a12a7eb7e6d1494
RazeSoldier52 <155234831+RazeSoldier52@users.noreply.github.com>
2025-12-24 15:50:04 +0300

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
-
 public class PlayerController : MonoBehaviour, IBoundaryBehaviour
 {
 
