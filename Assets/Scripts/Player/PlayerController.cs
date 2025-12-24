0b3f9d8258f1de6eb60909bd308f16403ceaff29
RazeSoldier52 <155234831+RazeSoldier52@users.noreply.github.com>
2025-12-24 17:27:55 +0300

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -29,6 +29,8 @@ public class PlayerController : MonoBehaviour, IBoundaryBehaviour
     [SerializeField] LayerMask groundLayer;
     [SerializeField] private Vector2 groundNormal;
     [SerializeField] private float minNormalYThreshold = 0.7f;
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
     [Header("Jumping")]
     [SerializeField] float jumpingPower;
     [SerializeField] private float minGroundedTime = 0.1f;
@@ -109,6 +111,8 @@ public class PlayerController : MonoBehaviour, IBoundaryBehaviour
                 rb.AddForce(Vector2.down * stickToGroundForce, ForceMode2D.Force);
             }
         }
+        xSpeed = rb.linearVelocity.x;
+        ySpeed = rb.linearVelocity.y;
     }
     private void Update()
     {
