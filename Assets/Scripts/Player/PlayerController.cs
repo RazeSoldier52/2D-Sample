6cf901b3f2a41ee879b3e8ed3b7850fd04b75dcf
RazeSoldier52 <155234831+RazeSoldier52@users.noreply.github.com>
2025-12-28 14:17:27 +0300

    Enemy Sprite Changed, Still Needs Adjustment

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -32,7 +32,6 @@ public class PlayerController : MonoBehaviour, IBoundaryBehaviour
     [Header("Jumping")]
     [SerializeField] private float jumpingPower;
     [SerializeField] private float minGroundedTime = 0.1f;
-    [SerializeField] private float jumpCutOffSpeedMultiplier = 0.5f;
     private float groundedTimeCounter = 0f;
     [Header("Coyote Time")]
     [SerializeField] private float coyoteTime = 0.2f;
@@ -162,14 +161,14 @@ public class PlayerController : MonoBehaviour, IBoundaryBehaviour
             {
                 coyoteTimeCounter = 0f;
                 groundedTimeCounter = 0f;
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+                if(rb.linearVelocity.y<0f) rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f); ;
                 rb.AddForce(groundNormal * jumpingPower * rb.mass, ForceMode2D.Impulse);
             }
         }
-        if (context.canceled && rb.linearVelocity.y > 0)
+        /*if (context.canceled && rb.linearVelocity.y > 0)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * jumpCutOffSpeedMultiplier);
-        }
+        }*/
     }
     public void Sprint(InputAction.CallbackContext context)
     {
